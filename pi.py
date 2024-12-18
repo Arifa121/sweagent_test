0ad13eaec9a2ab241005fc2174cc65568089b35e
ARIFA M Y <136242787+Arifa121@users.noreply.github.com>
2024-12-18 11:03:12 +0530

    Create pi.py

---
+++ pi.py
@@ -0,0 +1,17 @@
+"""
+This module only gets Pi.
+"""
+
+
+def get_pi() -> float:
+    """
+    This dummy evaluation returns a slice of Pi.
+
+    Returns
+    -------
+
+    float
+        The first couple of digits of Pi.
+    """
+
+    return 3.1415926535
